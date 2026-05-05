e67f592d5cd5421e042a697ee67192cd5a585bff
shubhamrasal <shubhamdharmarasal@gmail.com>
2026-05-05 20:04:36 +0530

    add marshal/unmarshal example

--- http-server/main.go
+++ http-server/main.go
@@ -1,22 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
+type UserInfo struct {
+	Username string `json: "username"`
+	Password string `json: "password"`
+	Location string `json: "location"`
+}
+
+type Result struct {
+	English string `json: "english"`
+	Maths   string `json: "maths"`
+	Total   int    `json: "total"`
+}
+
+func login(userdata UserInfo) (Result, error) {
+
+	fmt.Printf("%+v\n", userdata)
+
+	var result Result
+	// logic to check username and password is correct or not
+	if userdata.Username == "admin" && userdata.Password == "admin" {
+
+		result.English = "90"
+		result.Maths = "80"
+		result.Total = 170
+
+		return result, nil
+
+	}
+	return result, fmt.Errorf("error: %s", "user is not admin")
+
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Println("==== Incoming request ==== ")
-	fmt.Println("method: ", r.Method)
-	fmt.Println("path: ", r.URL.Path)
-	fmt.Println("query: ", r.URL.RawQuery)
-	fmt.Println("headers: ", r.Header)
-	fmt.Println("content length: ", r.ContentLength)
-	fmt.Println("host: ", r.Host)
-	fmt.Println("body: ", r.Body)
-	// read the body
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		fmt.Println("error reading body: ", err)
@@ -24,8 +47,32 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Println("body data: ", string(body))
 
-	w.WriteHeader(400)
-	fmt.Fprintln(w, "Helo From http-server demo")
+	// we want to store body data into userInfo
+	var adminUser UserInfo
+	err = json.Unmarshal(body, &adminUser)
+	if err != nil {
+		fmt.Println("ERROR:", err.Error())
+	}
+	result, err := login(adminUser)
+	if err != nil {
+
+		fmt.Println("ERROR: ", err.Error())
+		w.WriteHeader(400)
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	// login successfull
+	w.WriteHeader(200)
+	// we want to send result in json format
+	// marshalling
+	resultJson, err := json.Marshal(result)
+	if err != nil {
+		fmt.Println("error: ", err)
+		return
+	}
+	fmt.Fprintln(w, string(resultJson))
+
 }
 
 func main() {
